49aca740b3ee3a13539b3ba91c2a0fb15f866fb6
Samlegamer <samlegamer.yt@gmail.com>
2024-08-22 03:13:15 +0200

    byg update
    fabric 1.19.2/3/4
    forge 1.18.2-1.16.5

---
+++ 1.16.5/Forge/Macaw's Fences - BYG/src/main/java/fr/samlegamer/mcwfencesbyg/Client.java
@@ -0,0 +1,43 @@
+package fr.samlegamer.mcwfencesbyg;
+
+import fr.samlegamer.mcwfencesbyg.block.MFBYGBlocksRegistry;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItem;
+import net.minecraft.world.FoliageColors;
+import net.minecraft.world.biome.BiomeColors;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+@Mod.EventBusSubscriber(modid = McwFencesBYG.MODID, bus = Bus.MOD, value = Dist.CLIENT)
+public class Client
+{
+	@SubscribeEvent
+	public static void colorsBlock(ColorHandlerEvent.Block e)
+    {
+    	/*
+    	 * Thanks Potion Studios
+    	 * https://github.com/Potion-Studios/Oh-The-Biomes-Weve-Gone/blob/master/Forge/src/main/java/net/potionstudios/biomeswevegone/forge/client/BiomesWeveGoneClientForge.java#L83
+    	 * only the parameters have been recovered to match the original leaves
+    	 */
+        e.getBlockColors().register((state, view, pos, tintIndex) -> view != null && pos != null ? BiomeColors.getAverageFoliageColor(view, pos) : FoliageColors.get(0.5D, 1.0D), 
+        		MFBYGBlocksRegistry.mahogany_hedge.get(), 
+        		MFBYGBlocksRegistry.maple_hedge.get());
+    }
+    
+	@SubscribeEvent
+	public static void colorsItem(ColorHandlerEvent.Item e)
+    {
+    	/*
+    	 * Thanks Potion Studios
+    	 * https://github.com/Potion-Studios/Oh-The-Biomes-Weve-Gone/blob/master/Forge/src/main/java/net/potionstudios/biomeswevegone/forge/client/BiomesWeveGoneClientForge.java#L95
+    	 * only the parameters have been recovered to match the original leaves
+    	 */
+        e.getItemColors().register((stack, tintIndex) -> {
+        	BlockState state = ((BlockItem) stack.getItem()).getBlock().defaultBlockState();
+            return e.getBlockColors().getColor(state, null, null, tintIndex);
+        }, MFBYGBlocksRegistry.mahogany_hedge.get(), MFBYGBlocksRegistry.maple_hedge.get());
+    }
+}
